27b9d284616c0c131f9e5a92fdc20c2ebe1ad35d
Freddie Reschwamm <fredreschwamm@outlook.com>
2021-08-02 11:54:03 +0100

    created basic html structure

---
+++ index.php
@@ -0,0 +1,32 @@
+
+
+
+
+<html>
+
+ <div >
+  <ul>
+   <li>Home</li>
+   <li>Add new</li>
+  </ul>
+ </div>
+ <div>
+  <img src="" alt="">
+  <p>Name</p>
+  <p>Cost</p>
+ </div>
+  <img src="" alt="">
+  <p>Name</p>
+  <p>Cost</p>
+ <div>
+  <img src="" alt="">
+  <p>Name</p>
+  <p>Cost</p>
+ </div>
+ <div>
+   <img src="" alt="">
+   <p>Name</p>
+   <p>Cost</p>
+ </div>
+
+</html>
