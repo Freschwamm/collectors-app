9b8c6a2947ad7d8652ceff098cb84093bc949e2b
Freddie Reschwamm <fredreschwamm@outlook.com>
2021-08-05 11:49:40 +0100

    updated accessibility

--- validation.php
+++ validation.php
@@ -3,16 +3,9 @@ $db = new PDO('mysql:host=db; dbname=pokemon-cards', 'root', 'password');
 
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
-$query = $db->prepare('SELECT * FROM `pokemon-card`;');
-
-$query->execute();
-
-$allPokemon = $query->fetchAll();
-
 if(isset($_POST)) {
     $allPokemonQuery = $db->prepare('INSERT INTO `pokemon-card` (`name`, `cost`, `image`, `rarity`)
                                             VALUES (:pokemonName,:pokemonCost, :pokemonImage,:pokemonRarity )');
-
     $allPokemonQuery->bindParam(':pokemonName', $_POST['pokemonName']);
     $allPokemonQuery->bindParam(':pokemonCost', $_POST['pokemonCost']);
     $allPokemonQuery->bindParam(':pokemonImage', $_POST['pokemonImage']);
@@ -20,6 +13,6 @@ if(isset($_POST)) {
 
     $allPokemonQuery->execute();
     header('Location: index.php');
-} 
+}
 
 
