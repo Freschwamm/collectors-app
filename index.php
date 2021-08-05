48aef0722b776edb6a4be1857285b1112aa950d7
Freddie Reschwamm <fredreschwamm@outlook.com>
2021-08-05 09:22:01 +0100

    form input created

--- index.php
+++ index.php
@@ -12,9 +12,12 @@ $query->execute();
 
 $allPokemon = $query->fetchAll();
 
-
-
-
+$allPokemonQuery = $db->prepare('INSERT INTO `pokemon-card` (`name`, `cost`, `image`, `rarity`)
+                                            VALUES ($_POST[pokemonName],
+                                                    $_POST[pokemonCost], 
+                                                    $_POST[pokemonImage], 
+                                                    $_POST[pokemonRarity] )');
+$allPokemonQuery->execute();
 
 ?>
 
@@ -29,7 +32,16 @@ $allPokemon = $query->fetchAll();
  <div>
  <?php echo pokemonCardContainer($allPokemon) ?>
  </div>
+ <div>
+     <form action="index.php" method="post">
+         <input type="text" placeholder="Input pokemon name" name="pokemonName">
+         <input type="text" placeholder="Input card cost" name="pokemonCost">
+         <input type="text" placeholder="Input pokemon card image link" name="pokemonImage">
+         <input type="text" placeholder="Input card rarity" name="pokemonRarity">
+         <input type="submit">
 
+     </form>
+ </div>
 
 </body>
 </html>
